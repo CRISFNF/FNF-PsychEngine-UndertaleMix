9ad74ecaf783289dc5f184a2dc84a01671eadfbd
CRISFNF <100472277+CRISFNF@users.noreply.github.com>
2022-12-06 09:51:43 -0300

    Delete assets/preload/data/Hypothermia directory

--- assets/preload/data/Hypothermia/script4.lua
+++
@@ -1,5 +0,0 @@
-function onUpdate() --script by https://squidbowl.carrd.co/
-
-    doTweenAlpha('AlphaTween1', 'timeBarBG', 0, 0.001) doTweenAlpha('AlphaTween2', 'timeBar', 0, 0.001) doTweenAlpha('AlphaTween3', 'timeTxt', 0, 0.001)
-    
-    end
